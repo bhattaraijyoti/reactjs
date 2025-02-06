bb03d4cc9a4c21ec281c4d9686787f12e98e8e4c
bhattaraijyoti <bhattataijyoti2115@gmail.com>
2025-02-06 20:40:38 +0545

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main>
+       <h1> Lets build a app</h1>
+
+      <Button>Click me</Button>
+     </main>  
+   
+  )
+}
+
+    
